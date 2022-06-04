5fa2f518bf8d4ddaa9151f158944d5fa7f8d1206
Luis fernando vega <lfexevega95@gmail.com>
2022-06-04 18:25:47 -0300

    Presentacion de pagina

--- index.js
+++ index.js
@@ -1,44 +1,111 @@
 const imagenes = [
-    {
-      src: './Assets/Lemon.jpg',
-      alt: 'Lemon',
-      nombre: 'Limones',
-      descripcion: 'Limones secuestrados de tafi viejo'
-    },
-    {
-      src: './Assets/Coffe.jpg',
-      alt: 'Coffe',
-      nombre: 'Cafe',
-      descripcion: 'Cafe cosechado por niños esclavos colombianos'
-    },
-    {
-      src: './Assets/Green-Coffe.jpg',
-      alt: 'Green_coffe',
-      nombre: 'Cafe Verde',
-      descripcion: 'Cafe extraido de las entrañas de hulk'
-    },
-  ];
-  var html = '';
-  function renderizarGaleria(imagenes) {
-    imagenes.forEach(function(imagen){
-      html += `
+  {
+    src: "./Assets/Lemon.jpg",
+    alt: "Green_coffe",
+    nombre: "Limon fresco",
+    descripcion: "Limones secuestrados de tafi viejo",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Green-Coffe.jpg",
+    alt: "Green_coffe",
+    nombre: "Cafe Verde",
+    descripcion: "Cafe extraido de las entrañas de hulk",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+  {
+    src: "./Assets/Coffe.jpg",
+    alt: "coffe",
+    nombre: "Cafe negro",
+    descripcion: "Cafe cosechado por niños esclavos colombianos",
+    ref: "./limon.html",
+  },
+];
+
+var html = "";
+function renderizarGaleria(imagenes) {
+  imagenes.forEach(function (imagen) {
+    html += `
         <div class="galeria-item">
-          <img src="${imagen.src}" alt="${imagen.alt}" width="200px" />
-          <h3>${imagen.nombre}</h3>
-          <p>${imagen.descripcion}</p>
+          <div class="Div_prod">
+          <img src="${imagen.src}" alt="${imagen.alt}" width="350em" height="300em" />
+          </div>
+          <div class="Div_info">
+          <h3 class="centrar">${imagen.nombre}</h3>
+          <p class="centrar">${imagen.descripcion}</p>
+          <a class="detalles" href="${imagen.ref}"><p>Detalles</p></a>
+          </div>
         </div>
       `;
-    });
-  };
-  renderizarGaleria(imagenes);
-  
+  });
+}
+renderizarGaleria(imagenes);
+document.getElementById("Product_container").innerHTML = html;
 
-//   let container = document.getElementById("Product_container");
-//   var htmlobject = document.createElement("div");
-//   htmlobject.innerHTML = html;
-//   console.log(htmlobject);
-//   container.appendChild(htmlobject);
-
-document.getElementById("Productcontainer").innerHTML = html;
-
- 
+{/* <div class="galeria-item">
+          <div class="Div_prod">
+          <img src="${imagen.src}" alt="${imagen.alt}" width="350em" height="300em" />
+          </div>
+          <div class="Div_info">
+          <h3 class="centrar">${imagen.nombre}</h3>
+          <p class="centrar">${imagen.descripcion}</p>
+          <a class="detalles" href="${imagen.ref}"><p class="centrar"><a href="#openModal" class="detalles">Lanzar el modal</a>
+          <div id="openModal" class="modalDialog">
+	    <div>
+		<a href="#close" title="Close" class="close">X</a>
+		<h2>Mi modal</h2>
+		<p>Este es un ejemplo de modal, creado gracias al poder de CSS3.</p>
+		<p>Puedes hacer un montón de cosas aquí, como alertas o incluso crear un formulario de registro aquí mismo.</p>
+	</div>
+</div></p></a>
+          </div>
+        </div> */}
